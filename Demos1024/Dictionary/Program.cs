855c3a6ad63c862f033a35a021842c9be1dfb6fd
Michael Hart <M.HART@students.clark.edu>
2019-10-24 14:53:38 -0700

    Done with demos

---
+++ Demos1024/Dictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaration
+            Dictionary<string, Student> studentDictionary =
+                new Dictionary<string, Student>();
+
+            // initialize (two different initialization methods shown here)
+            studentDictionary.Add("Mike", new Student("Mike1"));
+            studentDictionary["Jill"] = new Student("Jill2");
+            studentDictionary["Jack"] = new Student("Jack3");
+
+            // traversal
+            foreach(string key in studentDictionary.Keys)
+            {
+                Console.WriteLine(key);
+            }
+            Console.WriteLine();
+
+            foreach(Student s in studentDictionary.Values)
+            {
+                Console.WriteLine(s.Name);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Demos1024/Stack and Queue/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_and_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // stacks
+            // declaration
+            Stack<Student> stackOfStudents = new Stack<Student>();
+
+            // push object onto stack (initialize stack)
+            stackOfStudents.Push(new Student("Mike"));
+            stackOfStudents.Push(new Student("Jill"));
+            stackOfStudents.Push(new Student("Hell Face"));
+
+            // traversal
+            foreach(Student s in stackOfStudents)
+            {
+                Console.WriteLine(s.Name);
+            }
+            Console.WriteLine(stackOfStudents.Count);
+            Console.WriteLine();
+
+            // pops
+            int size = stackOfStudents.Count;
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(stackOfStudents.Pop().Name);
+            }
+            Console.WriteLine(stackOfStudents.Count);
+            Console.WriteLine();
+
+            // queues
+            Queue<Student> studentQueue = new Queue<Student>();
+
+            // put items in the queue
+            studentQueue.Enqueue(new Student("Mike"));
+            studentQueue.Enqueue(new Student("Jill"));
+            studentQueue.Enqueue(new Student("Hell Face"));
+
+            // traversal
+            foreach(Student s in studentQueue)
+            {
+                Console.WriteLine(s.Name);
+            }
+            Console.WriteLine(studentQueue.Count);
+            Console.WriteLine();
+
+            size = studentQueue.Count;
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(studentQueue.Dequeue().Name);
+            }
+            Console.WriteLine(studentQueue.Count);
+            Console.WriteLine();
+        }
+    }
+}
